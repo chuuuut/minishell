30dc09796135e300b4cc8739a90edaeed4e5de16
Thomas Cali <tcali@f6r2s1.clusters.42paris.fr>
2025-06-05 20:16:34 +0200

    started parsinf fct, need to define and declare ft_strndup.

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 19:26:29 by tcali             #+#    #+#             */
-/*   Updated: 2025/06/05 19:57:38 by tcali            ###   ########.fr       */
+/*   Updated: 2025/06/05 20:15:27 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 char	**parse_line(const char *line)
 {
-	char	**tokens;
+	char		**tokens;
+	const char	*start;
+	int			i;
+	int			len;
 
 	tokens = safe_malloc(sizeof(char) * 100);
+	i = 0;
+	len = 0;
+	while (*line)
+	{
+		while (*line && ft_isspace(*line))
+			line++;
+		if (!line)
+			break ;
+		start = line;
+		while (*line && !ft_isspace(*line))
+			line++;
+		len = line - start;
+		tokens[i] = ft_strndup(start, len);
+		i++;
+	}
+	tokens[i] = NULL;
 	return (tokens);
 }
