6534d722468a727365a11a36b9babe8f5332759e
tcali <tcali@student.42.fr>
2025-06-06 17:41:29 +0200

    added a part of the exec, adjusted slightly my pipiex, just to test execve, still a lot to do since I don't manage multiple cmds, nor redirections for now. Working good though. Also prgm quits after executing one command.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 18:30:16 by tcali             #+#    #+#             */
-/*   Updated: 2025/06/05 20:00:09 by tcali            ###   ########.fr       */
+/*   Updated: 2025/06/06 17:37:16 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,21 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef struct s_data
+{
+	int		pipe_fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	char	*cmd1;
+	char	*cmd2;
+	char	**envp;
+	char	*file1;
+	char	*file2;
+	int		nb_tokens;
+}		t_data;
+
 //parse.c
 char	**parse_line(const char *line);
 
+char	*ft_str_threejoin(char const *s1, char const *s2, char const *s3);;
 #endif
--- src/main.c
+++ src/main.c
@@ -6,29 +6,147 @@
 /*   By: tcali <tcali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 18:29:44 by tcali             #+#    #+#             */
-/*   Updated: 2025/06/06 16:03:09 by tcali            ###   ########.fr       */
+/*   Updated: 2025/06/06 17:39:08 by tcali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_tokens(char **tokens)
+//void	print_tokens(char **tokens)
+//{
+//	int	i;
+
+//	i = 0;
+//	while (tokens[i])
+//	{
+//		printf("tokens[%d] = [%s]\n", i, tokens[i]);
+//		i++;
+//	}
+//}
+
+void	init_data(t_data *data, char **tokens, char **env)
 {
 	int	i;
 
 	i = 0;
+	data->pid1 = 0;
+	data->pid2 = 0;
+	data->cmd1 = tokens[0];
+	data->cmd2 = NULL;
+	//if (data->cmd2 == NULL || data->cmd2[0] == '\0')
+	//{
+	//	write(2, "empty command\n", 22);
+	//	exit(EXIT_FAILURE);
+	//}
+	data->envp = env;
+	data->file1 = NULL;
+	data->file2 = NULL;
 	while (tokens[i])
-	{
-		printf("tokens[%d] = [%s]\n", i, tokens[i]);
 		i++;
+	data->nb_tokens = i;
+}
+
+void	free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free(array);
+}
+
+char	*find_command(char *cmd, char **path)
+{
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	while (path[i])
+	{
+		cmd_path = ft_str_threejoin(path[i], "/", cmd);
+		if (!cmd_path)
+			return (NULL);
+		if (access(cmd_path, F_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_command_path(char *cmd, char **env)
+{
+	char	**path;
+	char	*bin;
+	int		i;
+
+	if (cmd[0] == '/')
+	{
+		if (access(cmd, F_OK) == 0)
+			return (ft_strdup(cmd));
+		return (NULL);
+	}
+	i = 0;
+	while (ft_strnstr(env[i], "PATH=", 5) == 0)
+		i++;
+	path = ft_split(env[i] + 5, ':');
+	bin = find_command(cmd, path);
+	if (!bin)
+	{
+		free_array(path);
+		return (NULL);
+	}
+	return (bin);
+}
+
+void	execute_command(char *command, char **env)
+{
+	char	**args;
+	char	*path;
+
+	if (ft_strchr(command, ';') || ft_strchr(command, '&')
+		|| ft_strchr(command, '|') || ft_strchr(command, '*'))
+	{
+		args = malloc(sizeof(char *) * 4);
+		if (!args)
+			error_exit("malloc failed");
+		args[0] = ft_strdup("/bin/sh");
+		args[1] = ft_strdup("-c");
+		args[2] = ft_strdup(command);
+		args[3] = NULL;
+		path = ft_strdup("/bin/sh");
+	}
+	else
+	{
+		args = ft_split(command, ' ');
+		if (!args || !args[0])
+			error_exit("invalid command");
+		path = get_command_path(args[0], env);
+		if (!path)
+		{
+			ft_printf_fd(2, "%s : command not found\n", args[0]);
+			free_array(args);
+			exit(127);
+		}
+	}
+	if (execve(path, args, env) == -1)
+	{
+		if (path != args[0])
+			free(path);
+		free_array(args);
+		error_exit("error executing command");
 	}
 }
 
-int	main(void)
+int	main(int ac, char **av, char **envp)
 {
 	char	*line;
 	char	**tokens;
+	t_data	data;
 
+	(void)ac;
+	(void)av;
 	line = NULL;
 	tokens = NULL;
 	while (1)
@@ -47,9 +165,12 @@ int	main(void)
 			printf("Error parsing line\n");
 			break ;
 		}
-		printf("%s\n", line);
-		print_tokens(tokens);
+		//printf("%s\n", line);
+		//print_tokens(tokens);
+		init_data(&data, tokens, envp);
+		execute_command(data.cmd1, data.envp);
 		free(line);
+		free(tokens);
 	}
 	return (0);
 }
